a793d21d874896ec1cf644a5ee29ac37d0a69683
Richard Darst <rkd@zgib.net>
2021-02-22 16:53:40 +0200

    commit -a of 2 files at Mon 22 Feb 2021 04:53:40 PM EET

---
+++ config.py
@@ -0,0 +1,60 @@
+mic = Selector(t='source', name='USB_Advanced', it='*')
+hdmi = Selector(t='sink', name='hdmi')
+hdmi_all = Selector(t='sink', name='hdmi', it='*')
+headphones = Selector(t='sink', name='HyperX')
+headphones_all = Selector(t='sink', name='HyperX', it='*')
+
+# Since buttons can have different MIDI note events on different channels, this
+# serves as a map between physical button number and note event, per-channel.
+# I could also re-program the MIDI device, but I don't want to do that just
+# yet.
+BUTTONMAP = {
+    # channel: [note_for_button_id]
+    0: [None, 36, 37, 38, 39, 40, 41, 42, 43],
+    1: [None, 35, 36, 42, 39, 37, 38, 46, 44],
+    2: [None, 60, 62, 64, 65, 67, 69, 71, 72],
+    3: [None, 36, 38, 40, 41, 43, 45, 47, 48],
+    }
+
+# Program dispatches here.
+DISPATCHERS = [
+    # Microphones
+    # toggle:
+    #(Dispatch(t=ON, ch=0, n=43), partial(mute, sel=mic)),
+    #(Dispatch(t=ON, ch=0, b=4), partial(mute, sel=mic, state=False)),
+    #(Dispatch(t=OFF,ch=0, b=4), partial(mute, sel=mic, state=True)),
+    # PTT:
+    #(Dispatch(t=ON, ch=0, b=3), partial(mute, sel=mic, state=False)),
+    #(Dispatch(t=OFF,ch=0, b=3), partial(mute, sel=mic, state=True)),
+    # volume:
+    (Dispatch(t=CC,        c= 5), partial(volume, sel=mic)),
+
+    # Zoom microphone mute toggle
+    (Dispatch(t=ON, ch=0, b=4), partial(zoom_mute)),
+    (Dispatch(t=ON, ch=0, b=4), partial(teams_mute)),
+    (Dispatch(t=OFF,ch=0, b=4), partial(zoom_mute)),
+    (Dispatch(t=OFF,ch=0, b=4), partial(teams_mute)),
+    # Zoom video toggle
+    (Dispatch(t=OFF,ch=0, b=2), partial(zoom_video)),
+
+    # Volumes, speakers
+    (Dispatch(t=ON, ch=0, b= 8), partial(mute, sel=hdmi)),
+    (Dispatch(t=CC,       c= 1), partial(volume, sel=hdmi_all, low=0, high=.7)),
+    (Dispatch(t=CC,       c= 1), partial(volume, sel=headphones_all, low=0, high=.7)),
+    (Dispatch(t=CC, ch=0, c= 2), partial(volume, sel=hdmi_all._replace(last=True), low=0, high=.7)),
+    (Dispatch(t=CC, ch=0, c= 3), partial(volume, sel=hdmi_all._replace(last=False), low=0, high=.7)),
+
+    # Moving speakers between sources
+    (Dispatch(t=ON, ch=3, b= 1), partial(pulse_move, sel=Selector(t='sink', it='*'), move_to=hdmi)),
+    (Dispatch(t=ON, ch=3, b= 5), partial(pulse_move, sel=Selector(t='sink', it='*'), move_to=headphones)),
+
+
+    # Camera exposure
+    (Dispatch(t=ON, ch=0, b= 1), partial(camera_exposure_auto)),
+    (Dispatch(t=CC, ch=0, c= 6), partial(camera_exposure)),
+
+    #(Dispatch(t=ON, ch=1, b= 1), partial(obs_switch, scene='Title card')),
+    #(Dispatch(t=ON, ch=1, b= 5), partial(obs_switch, scene='Gallery')),
+    #(Dispatch(t=ON, ch=1, b= 2), partial(obs_switch, scene='Desktop (local)+camera')),
+    #(Dispatch(t=ON, ch=1, b= 6), partial(obs_switch, scene='Desktop (remote)+camera (1)')),
+    ]
--- midictl.py
+++ midictl.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 import sys
 import time
+import traceback
 
 import mido
 import pulsectl
@@ -13,12 +14,20 @@ import pulsectl
 P = pulsectl.Pulse()
 
 
+
 def listen(argv):
 #    input_name = list(filter(lambda x: "lpd8" in x.lower(), mido.get_input_names()))[0]
 #    with mido.open_input(input_name) as port:
     parser = argparse.ArgumentParser()
     parser.add_argument('device_file', nargs='?', default=glob.glob('/dev/midi*')[0])
+    parser.add_argument('--config', '-c', nargs='*', default=['config.py'])
     args = parser.parse_args()
+
+    CONFIG_MODIFICATION_TIME = os.stat(__file__).st_mtime
+
+    for configfile in args.config:
+        exec(open(configfile).read(), globals())
+        CONFIG_MODIFICATION_TIME = max(CONFIG_MODIFICATION_TIME, os.stat(configfile).st_mtime)
 
     f = open(args.device_file, 'rb')
 
@@ -38,6 +47,14 @@ def listen(argv):
             print(msg)
             handle(msg)
 
+            # Restart script if config has been modified
+            mtime = max(os.stat(fname).st_mtime
+                        for fname in [__file__] + args.config)
+            if mtime > CONFIG_MODIFICATION_TIME:
+                f.close()
+                print("Restarting to reload config...")
+                os.execv(sys.executable, [sys.executable] + sys.argv)
+
 
 def handle(msg):
     for dis, func in DISPATCHERS:
@@ -48,7 +65,10 @@ def handle(msg):
           and (dis.p  is None or dis.p == getattr(msg, 'program', None)) \
           and (dis.b  is None or BUTTONMAP[msg.channel][dis.b] == getattr(msg, 'note', None)) \
           :
-            print('  -->', func)
+            if isinstance(func, partial):
+                print("  -->", func.func.__name__, func.args, func.keywords)
+            else:
+                print("  -->", func.__name__)
             func(msg)
 
 
@@ -107,6 +127,19 @@ def volume(msg, sel, low=0, high=1):
     for source in find_pulse(sel):
         volume = low + (high-low)*msg.value/127
         P.volume_set_all_chans(source, volume)
+
+
+def pulse_move(msg, sel, move_to):
+    speaker = next(iter(find_pulse(move_to)))
+    print(speaker)
+    for source in find_pulse(sel):
+        print(source)
+        try:
+            P.sink_input_move(source.index, speaker.index)
+        except:
+            traceback.print_exc()
+    P.sink_default_set(speaker)
+
 
 # v4l cameras exposures
 #  Requirements: v4l2-ctl command line utility installed (debian: v4l-utils)
@@ -182,60 +215,8 @@ ON = 'note_on'
 OFF = 'note_off'
 CC = 'control_change'
 PC = 'program_change'
-mic = Selector(t='source', name='USB_Advanced', it='*')
-hdmi = Selector(t='sink', name='hdmi')
-hdmi_all = Selector(t='sink', name='hdmi', it='*')
-headphones = Selector(t='sink', name='HyperX', it='*')
 
-# Since buttons can have different MIDI note events on different channels, this
-# serves as a map between physical button number and note event, per-channel.
-# I could also re-program the MIDI device, but I don't want to do that just
-# yet.
-BUTTONMAP = {
-    # channel: [note_for_button_id]
-    0: [None, 36, 37, 38, 39, 40, 41, 42, 43],
-    1: [None, 35, 36, 42, 39, 37, 38, 46, 44],
-    2: [None, 60, 62, 64, 65, 67, 69, 71, 72],
-    3: [None, 36, 38, 40, 41, 43, 45, 47, 48],
-    }
 
-# Program dispatches here.
-DISPATCHERS = [
-    # Microphones
-    # toggle:
-    #(Dispatch(t=ON, ch=0, n=43), partial(mute, sel=mic)),
-    #(Dispatch(t=ON, ch=0, b=4), partial(mute, sel=mic, state=False)),
-    #(Dispatch(t=OFF,ch=0, b=4), partial(mute, sel=mic, state=True)),
-    # PTT:
-    #(Dispatch(t=ON, ch=0, b=3), partial(mute, sel=mic, state=False)),
-    #(Dispatch(t=OFF,ch=0, b=3), partial(mute, sel=mic, state=True)),
-    # volume:
-    (Dispatch(t=CC, ch=0, c= 5), partial(volume, sel=mic)),
-
-    # Zoom microphone mute toggle
-    (Dispatch(t=ON, ch=0, b=4), partial(zoom_mute)),
-    (Dispatch(t=ON, ch=0, b=4), partial(teams_mute)),
-    (Dispatch(t=OFF,ch=0, b=4), partial(zoom_mute)),
-    (Dispatch(t=OFF,ch=0, b=4), partial(teams_mute)),
-
-    (Dispatch(t=OFF,ch=0, b=2), partial(zoom_video)),
-
-    # Volumes, speakers
-    (Dispatch(t=ON, ch=0, b= 8), partial(mute, sel=hdmi)),
-    (Dispatch(t=CC, ch=0, c= 1), partial(volume, sel=hdmi_all, low=0, high=.7)),
-    (Dispatch(t=CC, ch=0, c= 1), partial(volume, sel=headphones, low=0, high=.7)),
-    (Dispatch(t=CC, ch=0, c= 2), partial(volume, sel=hdmi_all._replace(last=True), low=0, high=.7)),
-    (Dispatch(t=CC, ch=0, c= 3), partial(volume, sel=hdmi_all._replace(last=False), low=0, high=.7)),
-
-    # Camera exposure
-    (Dispatch(t=ON, ch=0, b= 1), partial(camera_exposure_auto)),
-    (Dispatch(t=CC, ch=0, c= 6), partial(camera_exposure)),
-
-    #(Dispatch(t=ON, ch=1, b= 1), partial(obs_switch, scene='Title card')),
-    #(Dispatch(t=ON, ch=1, b= 5), partial(obs_switch, scene='Gallery')),
-    #(Dispatch(t=ON, ch=1, b= 2), partial(obs_switch, scene='Desktop (local)+camera')),
-    #(Dispatch(t=ON, ch=1, b= 6), partial(obs_switch, scene='Desktop (remote)+camera (1)')),
-    ]
 
 
 if __name__ == "__main__":
